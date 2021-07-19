3a8d38eea62fb11c99c3582b1f85196145fc5b56
dsohrabian <dnsohrabian@gmail.com>
2021-07-19 10:12:06 -0400

    Weekly commit of scrapes. More docs. Changed charts to percents.

--- charts.py
+++ charts.py
@@ -9,12 +9,12 @@ df = pd.read_csv('CSU_parking_live.csv', index_col='date', parse_dates=True)
 cols = df.columns
 
 # Subscriptions
-select_col = [field for field in cols if 'spaces' in field if 'permit' in field]
+select_col = [field for field in cols if 'perc' in field if 'permit' in field]
 plotdf = df[select_col]
-dailymins = plotdf.resample('30T').min() # resample the data to 30 minute increments. recode to whatever is best for you scheduling
+dailymins = plotdf.resample('60T').min() # resample the data to 30 minute increments. recode to whatever is best for you scheduling
 dailymins.columns = ['South', 'Prospect', 'West', 'Central', 'East']
 ax = dailymins.plot(style='o-', ms=3.5, lw=1)
-ax.yaxis.set_major_formatter('{x:,g}')
+ax.yaxis.set_major_formatter('{x:.0%}')
 ax.set_ylabel('Garage Vacancy (Subscribers)')
 ax.set_xlabel('')
 ax.set_ylim(0)
@@ -22,12 +22,12 @@ plt.legend(bbox_to_anchor=(.5, .98), bbox_transform=plt.gcf().transFigure, ncol=
 plt.tight_layout()
 
 # Public spaces
-select_col = [field for field in cols if 'spaces' in field if 'public' in field]
+select_col = [field for field in cols if 'perc' in field if 'public' in field]
 plotdf = df[select_col]
-dailymins = plotdf.resample('30T').min()
+dailymins = plotdf.resample('60T').min()
 dailymins.columns = ['South', 'Prospect', 'West', 'Central', 'East']
 ax = dailymins.plot(style='o-', ms=3.5, lw=1)
-ax.yaxis.set_major_formatter('{x:,g}')
+ax.yaxis.set_major_formatter('{x:.0%}')
 ax.set_ylabel('Garage Vacancy (Open to Public)')
 ax.set_xlabel('')
 ax.set_ylim(0)

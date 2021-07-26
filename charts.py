e207a20bb74311e66d67c85c411cb200c5ad7cf2
dsohrabian <dnsohrabian@gmail.com>
2021-07-26 15:33:19 -0400

    Add new figure text and average open spaces

--- charts.py
+++ charts.py
@@ -15,24 +15,28 @@ parking_total = sum([611, 291, 600, 867, 600])  # total parking capacity per fac
 # data,i.e. 'South', 'Prospect', 'West', 'Central', 'East'
 plotdf = df[select_col].copy()
 plotdf['total_use'] = 1 - (plotdf.sum(axis=1) / parking_total)
+plotdf['total_open'] = plotdf.iloc[:, 0:-1].sum(axis=1)
+avg_empty = plotdf['total_open'].mean()
 
 # resample the data to 1 hour increments. recode to whatever is best for you scheduling
 hourlymins = plotdf.resample('1H').max()
-# percent of parking that is being used
-
-# rename columns and drop nulls
-hourlymins.dropna(inplace=True)
 
 # plot the final column percentages
-ax = hourlymins.iloc[:, -1].plot(style='-', ms=3.5, lw=2, legend=False)
+ax = hourlymins.loc[:, 'total_use'].plot(style='-', ms=3.5, lw=2, legend=False)
 
 # format
 ax.yaxis.set_major_formatter('{x:.0%}')
 locator = md.AutoDateLocator()
-ax.xaxis.set_major_formatter(md.DateFormatter('%b %d'))
+# ax.xaxis.set_major_formatter(md.DateFormatter('%b %d'))
 ax.set_ylabel('% Occupied Space')
 ax.set_xlabel('')
 ax.set_ylim(0, 1)
+plt.text(.5, .9, f'{parking_total:,d}' + ' spaces total', ha='center', transform=ax.transAxes,
+         bbox={'boxstyle': 'round', 'color': '.8'})
+plt.text(.8, .9, f'{avg_empty:,.0f} avg. empty', c='red', ha='center', transform=ax.transAxes,
+         bbox={'boxstyle': 'round', 'color': '.8'})
 
-plt.suptitle(f'South, Prospect, West, Central, East Garages')
+average_spaces = hourlymins
+
+plt.suptitle(f'All Garages (South, Prospect, West, Central, East)')
 plt.tight_layout()

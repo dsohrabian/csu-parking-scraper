7ae1a024a1d4f556fd2c16a1b4ac2c65dfa596f5
dsohrabian <dnsohrabian@gmail.com>
2021-07-29 12:18:33 -0400

    Tweak capital cost with fewer boxes. New rows

--- README.md
+++ README.md
@@ -6,12 +6,11 @@ funds that can go to more active uses for students and the built environment of 
 it subsidizes and encourages unsustainable transportation patterns, and after all this, it sits empty most of the time.
 
 At about $20,000 per space for construction cost, the five garages that are scraped here probably cost
-somewhere around **$60 million** dollars to build; they also have debt to be paid and millions in operating costs.
+somewhere around **$60 million** dollars to build. They also have debt service payments and millions in operations costs
+that consume most of the parking revenues. 
 
 
 [Why America Has So Many Empty Parking Spaces](https://www.mentalfloss.com/article/503014/why-america-has-so-many-empty-parking-spaces)
-
-[The High Cost of Free Parking](https://www.youtube.com/watch?v=Akm7ik-H_7U&feature=emb_title)
 
 ## Summary 
 * A simple scraper script that obtains parking availability numbers from 
@@ -21,8 +20,8 @@ somewhere around **$60 million** dollars to build; they also have debt to be pai
 
 * Still testing accuracy, consistency and usefulness of source.
     
-* The main issue on this data source is whether capacity is dictated by an arbitrary number of managed spaces or if it refl
-  ects actual physical capacity.  
+* The main issue on this data source is whether capacity is dictated by an arbitrary number of managed spaces or if it 
+  reflects actual physical capacity.  
   According to the [CSU sites for each garage](https://www.csuohio.edu/parking/west-garage) there is the following
   amount of parking capacity:
   * 611 spaces in South Garage
--- charts.py
+++ charts.py
@@ -4,8 +4,10 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as md
 import matplotlib.ticker
 import pandas as pd
+import matplotlib as mpl
 
 plt.style.use('ggplot')
+mpl.rcParams['font.family'] = 'Segoe UI'
 df = pd.read_csv('CSU_parking_live.csv', index_col='date', parse_dates=True)
 cols = df.columns
 
@@ -17,6 +19,7 @@ plotdf = df[select_col].copy()
 plotdf['total_use'] = 1 - (plotdf.sum(axis=1) / parking_total)
 plotdf['total_open'] = plotdf.iloc[:, 0:-1].sum(axis=1)
 avg_empty = plotdf['total_open'].mean()
+construction_cost = avg_empty * 20000 / 1000000  # cost of empty spaces in millions
 
 # resample the data to 1 hour increments. recode to whatever is best for you scheduling
 hourlymins = plotdf.resample('1H').max()
@@ -28,16 +31,17 @@ ax = hourlymins.loc[:, 'total_use'].plot(style='-', ms=3.5, lw=2, legend=False)
 ax.yaxis.set_major_formatter('{x:.0%}')
 locator = md.AutoDateLocator()
 # ax.xaxis.set_major_formatter(md.DateFormatter('%b %d'))
-ax.set_ylabel('% Occupied Space')
+ax.set_ylabel('% Occupied Parking')
 ax.set_xlabel('')
 ax.set_ylim(0, 1)
-plt.text(.2, .9, f'{parking_total:,d}' + ' spaces total', ha='center', transform=ax.transAxes,
-         bbox={'boxstyle': 'round', 'color': '.8'})
-plt.text(.8, .9, f'{avg_empty:,.0f} avg. empty', c='blue', ha='center', transform=ax.transAxes,
-         bbox={'boxstyle': 'round', 'color': '.8'})
-plt.text(.8, .82, f'${avg_empty*20000/1000000:.1f}m unused capital cost', c='red', ha='center', transform=ax.transAxes,
-         bbox={'boxstyle': 'round', 'color': '.8'})
+plt.text(.65, .825, f'{parking_total:,d}' + ' spaces total' + '\n'
+         + f'{avg_empty:,.0f} avg. spaces empty' + '\n'
+         + f'${construction_cost:.1f}m unused capital cost*',
+         ha='left', linespacing=1.5, transform=ax.transAxes,)
+plt.text(.65, .78, '*Based on $20k per space construction cost', color='.25',
+         ha='left', fontsize=6.5, linespacing=1.5, transform=ax.transAxes,)
 
+bbox = {'boxstyle': 'round', 'color': '.8'}
 average_spaces = hourlymins
 
 plt.suptitle(f'All Garages (South, Prospect, West, Central, East)')

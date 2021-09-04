37cc302d131f721085bb2059e4b68a1098382c82
dnsohrab <dnsohrabian@gmail.com>
2021-09-03 23:00:04 -0400

    avg lines colors

--- charts.py
+++ charts.py
@@ -24,10 +24,13 @@ construction_cost = avg_empty * 20000 / 1000000  # cost of empty spaces in milli
 
 # resample the data to 1 hour increments for more consistent sampling in case scrapes are missed
 hourlymins = plot_df.resample('1H').max()
+off_season_hrs = hourlymins[hourlymins.index <= '2021-08-22']
+in_school_hrs = hourlymins[hourlymins.index > '2021-08-22']
 
 # plot the final column percentages
-fig, ax = plt.subplots(figsize=[9, 4.8])
-ax.plot('percent_use', '-', ms=3.5, lw=1, data=hourlymins)
+fig, ax = plt.subplots(figsize=[15, 10], dpi=300)
+ax.plot('percent_use', '-', ms=1, lw=1, data=off_season_hrs, c='tab:blue')
+ax.plot('percent_use', '-', ms=1, lw=1, data=in_school_hrs, c='tab:red')
 
 # # format the plot
 # x axis major ticks as months
@@ -54,13 +57,13 @@ ax.yaxis.set_major_formatter('{x:.0%}')
 
 
 # TODO add average use lines for in season and summer season separately
-average_use_break = hourlymins[hourlymins.index < '2021-08-20']['percent_use'].mean()
-average_use_school = hourlymins[hourlymins.index > '2021-08-23']['percent_use'].mean()
+average_use_break = off_season_hrs['percent_use'].mean()
+average_use_school = in_school_hrs['percent_use'].mean()
 
 xalign = 0.05
 plt.text(xalign, .825, f'{capacity_total:,d}' + ' spaces total' + '\n'
          + f'{average_use_school:,.0%} avg.use during school' + '\n' + f'{average_use_break:,.0%} avg.use during break',
-         ha='left', linespacing=1.5, transform=ax.transAxes )
+         ha='left', linespacing=1.5, transform=ax.transAxes, fontsize='7' )
 
 # off peak average line
 plt.axhline(average_use_break, ls=':', lw=2, c='tab:blue', alpha=.75)
@@ -69,14 +72,14 @@ plt.annotate(f'{average_use_break:.0%} avg. hourly use during break', xy=(date_m
              arrowprops={'arrowstyle': '->', 'connectionstyle': "angle,angleA=180,angleB=90", 'color': 'tab:blue',
                          'lw': '.5', 'alpha': 0.75})
 # school season line
-plt.axhline(average_use_school, ls=':', lw=2, c='tab:orange', alpha=.75)
+plt.axhline(average_use_school, ls=':', lw=2, c='tab:red', alpha=.75)
 plt.annotate(f'{average_use_school:.0%} avg. hourly use during class', xy=(date_min + 20, average_use_school),
-             xytext=(date_min + 10, .55), c='tab:orange',
-             arrowprops={'arrowstyle': '->', 'connectionstyle': "angle,angleA=180,angleB=90", 'color': 'tab:orange',
+             xytext=(date_min + 10, .55), c='tab:red',
+             arrowprops={'arrowstyle': '->', 'connectionstyle': "angle,angleA=180,angleB=90", 'color': 'tab:red',
                          'lw': '.5', 'alpha': 0.75})
 
-plt.text(.5, 1.01, f'{date_min+2} to {date_max-2}', size=7, c='gray', ha='center', transform= ax.transAxes)
+plt.text(.5,.9 , f'{date_min+2} to {date_max-2}', size=7, c='gray', ha='center', transform= ax.transAxes)
 
-plt.suptitle(f'CSU Garages (South, Prospect, West, Central, East)')
+plt.suptitle(f'CSU Garages (South, Prospect, West, Central, East)', size=8)
 plt.tight_layout()
-plt.savefig('./exports/ex1.png', dpi=150, )
+plt.savefig('./exports/ex1.png', bbox_inches = 'tight' )
